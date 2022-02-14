340db4694d922db64922671648641cd8e6600753
Aryan Agrawal <alemantrixaryan@gmail.com>
2022-02-15 02:52:52 +0530

    server is connected to the client

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const app = express();
+const http = require("http");
 const path = require("path");
+const socketio = require("socket.io");
 
 const port = process.env.PORT || 3000;
 const publicDirectory = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectory));
+const server = http.createServer(app);
+const io = socketio(server);
+// above is some configuration
 
-app.get("/", (req, res) => {});
+io.on("connection", () => {
+  console.log("new connection right here");
+});
 
 // lisening to port
-app.listen(port, () => {
+server.listen(port, () => {
   console.log("connected to port:", port);
 });
