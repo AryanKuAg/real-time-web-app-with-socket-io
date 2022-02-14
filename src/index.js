8610adeba4ceef6270d76a8f496b2d82898a34b6
Aryan Agrawal <alemantrixaryan@gmail.com>
2022-02-15 01:54:52 +0530

    All the challenges completed

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const port = 3000;
+const port = process.env.PORT || 3000;
+const publicDirectory = path.join(__dirname, "../public");
 
-app.get("/", (req, res) => {
-  const indexPath = path.join(__dirname, "../public/index.html");
-  res.sendFile(indexPath);
-});
+app.use(express.static(publicDirectory));
+
+app.get("/", (req, res) => {});
 
 // lisening to port
 app.listen(port, () => {
